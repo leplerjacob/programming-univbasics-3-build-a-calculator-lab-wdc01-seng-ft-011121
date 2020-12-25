4e1bc2250b1f5e712414e920782d53fc406ed246
Jacob Lepler <lepler.jacob@gmail.com>
2020-12-25 02:03:27 +0000

    Done.

--- lib/math.rb
+++ lib/math.rb
@@ -1,23 +1,40 @@
 def addition(num1, num2)
-
+  puts num1 + num2
+  num1 + num2
 end
 
 def subtraction(num1, num2)
-
+  puts num1 - num2
+  num1 - num2
 end
 
 def division(num1, num2)
-
+  puts num1 / num2
+  num1 / num2
 end
 
 def multiplication(num1, num2)
-
+  puts num1 * num2
+  num1 * num2
 end
 
 def modulo(num1, num2)
-
+  puts num1 % num2
+  num1 % num2
 end
 
 def square_root(num)
-
+  puts Math.sqrt(num)
+  Math.sqrt(num)
 end
+
+num1 = 5
+num2 = 6
+
+addition(num1, num2)
+subtraction(num1, num2)
+division(num1, num2)
+multiplication(num1, num2)
+modulo(num1, num2)
+square_root(num1)
+
